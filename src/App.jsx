f6cdf3d453a759fa77481d2b6ea744646a10fc11
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-07-12 20:36:49 +0100

    sass initiated with a variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import "./myapp.scss"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="text-color">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
