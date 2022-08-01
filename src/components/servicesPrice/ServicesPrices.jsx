7e4c81d132a8bb8de1f418d0b2194e213f49f8a1
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-08-01 15:20:22 +0100

    service page styled

--- src/components/servicesPrice/ServicesPrices.jsx
+++ src/components/servicesPrice/ServicesPrices.jsx
@@ -11,24 +11,24 @@ function ServicesPrices() {
           <Col className="v-h-center">
             <div>
                   <h1>Natural-Nails Services</h1>
-                  <p className="fs-">
+                  <p className="fs-5 w-75 m-auto">
                     At Natural Nails, we work hard to provide & satisfy our guests
                     with a high quality and service in relaxation place. We have skill
                     technicians with professional Nails treatment: Manicure, Pedicure,
                     shellac, Acrylic.
                   </p>
-                  <p className="fs-">
+                  <p className="fs-5 w-75 m-auto">
                     Thank you to all our lovely loyal clients for your support over
                     year! As ever, we want to provide the best products and safe
                     service we can, so there will be a small price increase on
                     treatments due to increasing of materials and fix cost.
                   </p>
-                  <p className="fs-">
+                  <p className="fs-5 w-75 m-auto">
                     we would like to inform you that our price of services will change
                     from 1st May 2022 onwards We greatly appreciate your support and
                     hope you understand the need to implement the increase.
                   </p>
-                  <p className="fs-">
+                  <p className="fs-5 w-75 m-auto">
                     Lots of love
                     <br />
                     Hanna and Natural Nail team,
@@ -36,15 +36,13 @@ function ServicesPrices() {
                   </div>
                 </Col>
               </Row>
-              {servicePricesData.map((item, i) => (<>
-              <div>
-                <hr/>
-                <h2>{item.icon} {item.title}</h2>
-                <hr/>
+              {servicePricesData.map((item, i) => (<div className="mt-5">
+              <div className=" ">
+                <h2 className="p-3">{item.icon} {item.title}</h2>
             </div>
-          <Row key={i} className="min-height-70">
-            <Col sm={12} md={6} className="v-h-center">
-              <div className="" style={{heith:"100%", width:"100%", overflow:"hidden"}}>
+          <Row key={i} className="min-height-70 border-top-1 border-bottom">
+            <Col sm={12} md={6} className="v-h-center ">
+              <div className="" style={{heith:"100%", width:"100%", overflow:"hidden",}}>
                 <img src={item.img || "https://via.placeholder.com/550x350/350"} alt="" className="h-100 w-100"/>
               </div>
             </Col>
@@ -59,7 +57,7 @@ function ServicesPrices() {
               </ListGroup>
             </Col>
           </Row>
-          </> ))
+          </div> ))
         }
       </Container>
     </div>
